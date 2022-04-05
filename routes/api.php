bd021f198e6eb776331f0d2a21ba9a88e2ea6ad5
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-04-05 12:25:35 +0400

    + api routes for image manipulation

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\AlbumController;
+use App\Http\Controllers\V1\ImageManipulationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('album', AlbumController::class);
+
+    Route::get('image', [ImageManipulationController::class, 'index']);
+    Route::get('image/{image}', [ImageManipulationController::class, 'show']);
+    Route::get('image/by-album/{album}', [ImageManipulationController::class, 'byAlbum']);
+    Route::post('image/resize', [ImageManipulationController::class, 'resize']);
+    Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
 });
