f68147f499338151c51c5c624d2626f0931fbbb9
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-04-05 12:27:09 +0400

    + ImageManipulation model + necessary fillables + constants

--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class ImageManipulation extends Model
 {
     use HasFactory;
+
+    const TYPE_RESIZE = 'resize';
+    const UPDATED_AT = false;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'output_path',
+        'type',
+        'data',
+        'album_id',
+        'user_id'
+    ];
 }
