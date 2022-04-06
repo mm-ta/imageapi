a905913abc911f1712015c629293d0c184c0b0ad
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-04-06 14:52:54 +0400

    + implement api resource for image manipulations

--- app/Http/Resources/V1/ImageManipulationResource.php
+++ app/Http/Resources/V1/ImageManipulationResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 
 class ImageManipulationResource extends JsonResource
 {
@@ -14,6 +15,16 @@ class ImageManipulationResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'name' => $this->name,
+            'original' => URL::to($this->path),
+            'output_path' => URL::to($this->output_path),
+            'data' => $this->data,
+            'user_id' => $this->user_id,
+            'album_id' => $this->album_id,
+            'created_at' => $this->created_at,
+        ];
     }
 }
